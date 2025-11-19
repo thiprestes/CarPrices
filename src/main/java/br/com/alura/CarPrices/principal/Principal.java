5e88b66fc5e3470a2c467db037997989011dfe09
Thiago Prestes <thiprestes_@hotmail.com>
2025-11-19 08:59:57 -0300

    Created Model Class and adjust Principal
    Created Model Class;
    Adjust Principal Class adding Brands request and Model request.

--- src/main/java/br/com/alura/CarPrices/principal/Principal.java
+++ src/main/java/br/com/alura/CarPrices/principal/Principal.java
@@ -1,16 +1,18 @@
 package br.com.alura.CarPrices.principal;
 
+import br.com.alura.CarPrices.model.BaseData;
+import br.com.alura.CarPrices.model.Model;
 import br.com.alura.CarPrices.service.ConsumingApi;
+import br.com.alura.CarPrices.service.DataConversion;
 
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Principal {
-
     private final String apiEndpoint = "https://fipe.parallelum.com.br/api/v2/";
-
     private ConsumingApi apiConsuming = new ConsumingApi();
-
     private Scanner readTerminal = new Scanner(System.in);
+    private DataConversion conversor = new DataConversion();
 
     public void showMenu() {
         var menu = """
@@ -35,6 +37,24 @@ public class Principal {
         }
 
         var json = apiConsuming.GetData(url);
+
+        var marcas = conversor.getList(json, BaseData.class);
+
+        marcas.stream()
+                .sorted(Comparator.comparing(BaseData::code))
+                .forEach(x -> System.out.println("nome da marca = " + x.name() + "; Código da Marca = " + x.code()));
+
+        System.out.println("Informe o código da marca para consulta: ");
+        var brandCode = readTerminal.nextLine();
+
+        url = url + "/" + brandCode + "/models";
+        json = apiConsuming.GetData(url);
+        var modelList = conversor.getData(json, Model.class);
+
+        System.out.println("modelos de carros");
+        modelList.models().stream()
+                .sorted(Comparator.comparing(BaseData::code))
+                .forEach(System.out::println);
 
     }
 }
